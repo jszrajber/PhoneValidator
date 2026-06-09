b5953382e2bdd46a3975792d2af9844275fc0a6b
kuba <jakubszrajber12@gmail.com>
2026-06-09 15:37:24 +0200

    Adding redis deps

--- backend/dependencies/db.py
+++ backend/dependencies/db.py
@@ -1,6 +1,20 @@
 from backend.db.session import AsyncSessionLocal
+from backend.core.config import settings
+import redis.asyncio as redis
+
+redis_pool = redis.ConnectionPool.from_url(
+    settings.REDIS_URL,
+    decode_responses=True,
+    socket_connect_timeout=5,
+    socket_timeout=5
+)
 
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+
+async def get_redis():
+    async with redis.Redis(connection_pool=redis_pool) as client:
+        yield client
